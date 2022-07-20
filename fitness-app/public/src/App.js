ed2bb1895990f3929ef0bb74c47aabb74cbc6024
Rahul gour <96679977+rahulgourvlog@users.noreply.github.com>
2022-07-20 18:45:05 +0530

    commit

---
+++ fitness-app/public/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Mainroutes from './Exercise-page/components/Mainroutes';
+
+function App() {
+  return (
+   <div>
+    <Mainroutes/>
+    </div>
+  );
+}
+
+export default App;
---
+++ fitness-app/public/src/Exercise-page/components/Mainroutes.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Dailyexercise from './dailyexercise';
+import Navbar from './Navbar';
+const Mainroutes = () => {
+  return (
+    <>
+     <div>
+<Navbar/>
+<Dailyexercise/>
+</div>
+    </>
+   
+  )
+}
+
+export default Mainroutes
