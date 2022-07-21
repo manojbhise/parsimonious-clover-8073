450fb298f3669a407a2958b34016ed820e1b3961
Rahul gour <96679977+rahulgourvlog@users.noreply.github.com>
2022-07-21 22:37:37 +0530

    commit

--- myfitnesspal/src/pages/exercise/Todayexercise.jsx
+++ myfitnesspal/src/pages/exercise/Todayexercise.jsx
@@ -1,19 +1,34 @@
-import React from "react";
-import { TbPencil } from "react-icons/tb";
+import React, { useState } from 'react'
+ import {TbPencil} from 'react-icons/tb';
+import Styles from "../../styling/Todayexercise.module.css"
 const Todayexercise = () => {
+  const [val,Setval]=useState("");
+  const [newval,setnewval]=useState(true)
+  const edittext=()=>{
+    setnewval(!newval)
+  }
   return (
-    <>
-      <div>
-        <div>
-          <div>Today's Exercise Notes</div>
-          <div>
-            Edit Note
-            <TbPencil />
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
+   <>
+   <div className={Styles.Today}>
+<div className={Styles.exe}>
+    <div className={Styles.cardios} >Today's Exercise Notes</div>
+    <div style={{ color:"#00548f"}} onClick={edittext}>{newval===true? "SaveNote":"Edit Note"}<TbPencil/></div>
+</div>
+<div className={Styles.divided}>
+  <p className={Styles.dividedpara}>
+  { !newval &&val}
+  {newval && <input className={Styles.input} type={"text"} value={val} onChange={(e)=>Setval(e.target.value)}/>}
+    
+  </p>
+  </div>
+  <div className={Styles.diva}></div>
+  <div  className={Styles.butt}>
+    <button className={Styles.button}>View Full Report(Printable)</button>
+  </div>
+   </div>
+   
+   </>
+  )
+}
 
-export default Todayexercise;
+export default Todayexercise
