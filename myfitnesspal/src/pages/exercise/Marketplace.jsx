1a1decdb156ae8e99bdc75afeff28a39367119eb
Manoj Bhise <bhise.manoj29@gmail.com>
2022-07-24 07:49:30 +0530

    exercise apps css ok

--- myfitnesspal/src/pages/exercise/Marketplace.jsx
+++ myfitnesspal/src/pages/exercise/Marketplace.jsx
@@ -1,38 +1,37 @@
 import React, { useState } from "react";
 import Styles from "../../styling/Marketplace.module.css";
-import appdata from "./data.json"
+import appdata from "./data.json";
 const Marketplace = () => {
-    const [getData,SetData]=useState(appdata.Apps1)
-    //console.log(appdata.Apps)
+  const [getData, SetData] = useState(appdata.Apps1);
+  //console.log(appdata.Apps)
   return (
     <>
       <div className={Styles.feacture}>
         <h2>App Marketplace</h2>
-        <div className={Styles.Apps} >
-{getData.map((elem,index)=>{
-    return <>
-   
-           <div key={elem.index} className={Styles.Apps1} >
-            <div>
-              <img  className={Styles.logo}src={elem.img} alt={"icon"} />
-            </div>
-           <div className={Styles.right}>
-              <p className={Styles.rightpara1}>{elem.p1}</p>
-       <p className={Styles.rightpara2}>{elem.Category}</p>
-              <a
-                href="http://www.myfitnesspal.com/fitbit/authorize"
-                rel="noreferrer"
-                target="_blank"
-              >
-                <button className={Styles.button}>GET</button>
-              </a>
-            </div>
-          </div>
-       
-    </>
-})}
-
-</div>     
+        <div className={Styles.Apps}>
+          {getData.map((elem, index) => {
+            return (
+              <>
+                <div key={elem.index} className={Styles.Apps1}>
+                  <div>
+                    <img className={Styles.logo} src={elem.img} alt={"icon"} />
+                  </div>
+                  <div className={Styles.right}>
+                    <p className={Styles.rightpara1}>{elem.p1}</p>
+                    <p className={Styles.rightpara2}>{elem.Category}</p>
+                    <a
+                      href="http://www.myfitnesspal.com/fitbit/authorize"
+                      rel="noreferrer"
+                      target="_blank"
+                    >
+                      <button className={Styles.button}>GET</button>
+                    </a>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
       </div>
     </>
   );
